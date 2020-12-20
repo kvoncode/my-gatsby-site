ec19c53f48e0e5db9924bf9845c425153ccaf1e6
kvoncode <kvoncode@gmail.com>
2020-12-20 10:35:08 +0900

    add: layout component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
-import * as React from "react"
-import { Link } from "gatsby";
-// markup
-const IndexPage = () => {
+import React from "react";
+import Layout from "../components/layout";
+
+export default function Home() {
   return (
-    <main>
-      <Link to="/contact/">Contact</Link>
-    </main>
+    <Layout>
+      <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
+      <p>
+        What do I like to do? Lots of course but definitely enjoy building
+        websites.
+      </p>
+    </Layout>
   );
 }
-
-export default IndexPage
